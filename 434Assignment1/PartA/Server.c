8d94a754af246277bb673e0c7a309d4d88cbbd9f
Matt <Matt@10.227.140.22>
2016-02-03 17:06:02 -0600

    Checking more.

--- 434Assignment1/PartA/Server.c
+++ 434Assignment1/PartA/Server.c
@@ -62,8 +62,8 @@ void add(char *key, char *value, int new_fd){
 void getValue(char *key, int new_fd){
 	int i = 0;
 	if(numUsed > 0){
-		printf("Before the for loop\n");
 		for(i = 0; i < 10; i++){
+			printf("Inside the for loop\n");
 			if(pairList[i].key != NULL && strcmp(pairList[i].key , key) == 0){
 				printf("Checking key: %s, value: %s\n", pairList[i].key, pairList[i].value);
 				char sent[MAXDATASIZE];
