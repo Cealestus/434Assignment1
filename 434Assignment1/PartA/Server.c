e0abe1013030bbaca34e77b47d39f8d7c6f31bef
Matt <Matt@10.227.140.22>
2016-02-03 18:12:30 -0600

    I don't even know anymore.

--- 434Assignment1/PartA/Server.c
+++ 434Assignment1/PartA/Server.c
@@ -50,8 +50,8 @@ void add(char *key, char *value, int new_fd){
 				return;
 			}
 			else if(pairList[i].key == NULL){
-				pairList[i].key = key;
-				pairList[i].value = value;
+				pairList[i].key = *key;
+				pairList[i].value = *value;
 				numUsed++;
 				char sent[MAXDATASIZE];
 				sprintf(sent, "Key: %s, Value: %s, added to the server, currently have %i values in the server\n", pairList[i].key, pairList[i].value, numUsed);
